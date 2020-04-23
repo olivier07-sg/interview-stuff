8f04dcf697b4d14c4dfc63b57b7cd75c4c29c060
Stephen Gardner <gardner.stephen892@gmail.com>
2020-04-23 19:32:57 +0100

    first

---
+++ src/main/java/com/interview/interviewstuff/InterviewStuffApplication.java
@@ -0,0 +1,13 @@
+package com.interview.interviewstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewStuffApplication.class, args);
+	}
+
+}
