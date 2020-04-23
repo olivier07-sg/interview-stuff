fc07f1ba2c035bee80a08fe3404f72c5020bb7d0
Jeno Premarajah <j13than@hotmail.com>
2020-04-23 21:28:26 +0100

    Jeno's Interview test

--- src/main/java/com/interview/interviewstuff/service/CardValidationService.java
+++ src/main/java/com/interview/interviewstuff/service/CardValidationService.java
@@ -1,5 +1,8 @@
 package com.interview.interviewstuff.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CardValidationService {
 
     /**
@@ -9,7 +12,41 @@ public class CardValidationService {
      * 4. If the total ends in zero, then the number is a valid credit card
      */
     public boolean isValid(String cardNumber) {
-        return false;
+
+        int count = 1;
+        int total  = 0;
+
+        for (int i = cardNumber.length() - 1; i >= 0; i--) {
+
+            int digit = Integer.parseInt(String.valueOf(cardNumber.charAt(i)));
+
+            if(count % 2 == 0) {
+                digit = doubleDigit(digit);
+            }
+
+            count++;
+
+            System.out.println(digit);
+
+            total += digit;
+        }
+
+        System.out.println(total);
+
+        String totalString = String.valueOf(total);
+        return totalString.charAt(totalString.length()-1) == '0';
+    }
+
+    private int doubleDigit(int digit) {
+        digit = digit * 2;
+
+        if (digit > 9) {
+            String stringDoubleDigit = String.valueOf(digit);
+            int firstDigit = Integer.parseInt(String.valueOf(stringDoubleDigit.charAt(0)));
+            int secondDigit = Integer.parseInt(String.valueOf(stringDoubleDigit.charAt(1)));
+            digit = firstDigit + secondDigit;
+        }
+        return digit;
     }
 
 }
--- src/test/java/com/interview/interviewstuff/CardValidationServiceTest.java
+++ src/test/java/com/interview/interviewstuff/CardValidationServiceTest.java
@@ -2,18 +2,34 @@ package com.interview.interviewstuff;
 
 import com.interview.interviewstuff.service.CardValidationService;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardValidationServiceTest {
 
     private static final String VALID_CARD_NUMBER = "4242424242424242";
 
+    private static final String VALID_CARD_NUMBER_2 = "5454545454545454";
+
     private static final String INVALID_CARD_NUMBER = "111111111111111";
 
+    @Inject
     private static CardValidationService cardNumberValidationService;
 
     @BeforeAll
     static void beforeAll() {
         cardNumberValidationService = new CardValidationService();
+    }
+
+    @Test
+    public void testIsValid() {
+        assertTrue(cardNumberValidationService.isValid(VALID_CARD_NUMBER));
+        assertTrue(cardNumberValidationService.isValid(VALID_CARD_NUMBER_2));
+        assertFalse(cardNumberValidationService.isValid(INVALID_CARD_NUMBER));
     }
 
     // TODO: Create tests below to test your service implementation
